81542267a1b8839de0be49fe95f949407d302bab
CCThompson82 <christophercthompson82@gmail.com>
2018-12-24 22:42:00 +0000

    Chore: Bookmark progress on client object

---
+++ src/clients/client_model.py
@@ -0,0 +1,50 @@
+"""
+Client that
+* abstracts dynamic model loading
+* provides a consistent API for training steps
+"""
+
+import os
+import sys
+import json
+from pydoc import locate
+import numpy as np
+
+WORK_DIR = os.environ['ROOT_DIR']
+sys.path.append(WORK_DIR)
+MODEL_CONFIG_PATH = os.path.join(WORK_DIR, 'config', 'model.json')
+HYPERPARAMS_CONFIG_PATH = os.path.join(WORK_DIR, 'config', 'hyperparams.json')
+
+
+class ModelClient(object):
+    def __init__(self, env_config):
+
+        with open(MODEL_CONFIG_PATH, 'r') as handle:
+            model_config = json.load(handle)
+
+        with open(HYPERPARAMS_CONFIG_PATH, 'r') as handle:
+            hyperparams_config = json.load(handle)
+
+        self.model = self.load_model(model_config=model_config,
+                                     hyperparams_config=hyperparams_config,
+                                     env_config=env_config)
+
+        # TODO: turn this into an object
+        self.state = {'step_count': np.zeros[env_config['nb_agents']],
+                      'episode_count': np.zeros[env_config['nb_agents']],
+                      'episode_score': np.zeros[env_config['nb_agents']]}
+
+    @staticmethod
+    def load_model(model_config, hyperparams_config, env_config):
+
+        model_name = model_config['model_name']
+        experiment_id = model_config['experiment']
+        overwrite_experiment = model_config['overwrite_experiment']
+
+        Model = locate('model.{}.model.Model'.format(model_name))
+        model = Model(experiment_id=experiment_id,
+                      overwrite_experiment=overwrite_experiment,
+                      nb_state_features=nb_state_features,
+                      nb_actions=nb_actions,
+                      hyperparams=hyperparams)
+        return model
